eca46add5d46cf0546ae9eeb3be1d6f90043d9ef
IAt0ny <toanpvnse03821@fpt.edu.vn>
2016-01-25 09:09:57 +0700

    Create getAttendees.py

---
+++ Eventbrite/getAttendees.py
@@ -0,0 +1,13 @@
+from requests import session
+
+OAuth = 'RXK2MITITUROVA2KII5A'
+e_id = '20799626242'
+
+url_prefix = 'https://www.eventbriteapi.com/v3/'
+
+with session() as s:
+ r = s.get(url_prefix+'events/'+e_id+'/attendees/?token='+'YWTHY73ZVEJOL6EVMZUF')
+ f = open('attendees.csv','w')
+ f.write('Email,Checkin_Status\n')
+ for i in r.json()['attendees']:
+ 	f.write( i['profile']['email'] + "," + str(i['barcodes'][0]['checkin_type']) + "\n")
